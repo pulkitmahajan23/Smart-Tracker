50ee486b03686aeb0d812527d164e717068fe263
Sakshi Sinha <43319913+sakshisinha13@users.noreply.github.com>
2020-09-22 10:00:17 +0530

    Vitals

---
+++ Healthapp/android/app/src/main/java/com/healthapp/metric/hr/HeartBeatMeasurerPackage.java
@@ -0,0 +1,41 @@
+package com.healthapp.metric.hr;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+public class HeartBeatMeasurerPackage implements ReactPackage {
+
+    private HeartBeatMeasurer heartBeatMeasurer;
+
+    @Nonnull
+    @Override
+    public List<ViewManager> createViewManagers(@Nonnull ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+
+    @Nonnull
+    @Override
+    public List<NativeModule> createNativeModules(@Nonnull ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(initHeartBeatMeasurer(reactContext));
+        return modules;
+    }
+
+    private HeartBeatMeasurer initHeartBeatMeasurer(ReactApplicationContext reactContext){
+        heartBeatMeasurer = new HeartBeatMeasurer(reactContext);
+        return heartBeatMeasurer;
+    }
+
+    public HeartBeatMeasurer getHeartBeatMeasurer() {
+        return heartBeatMeasurer;
+    }
+
+
+}
+
