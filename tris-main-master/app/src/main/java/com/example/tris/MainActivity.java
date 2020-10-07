4baca249f9ac77417eca3ccd1f0b5f603654d480
pulkitmahajan23 <pulkitmahajan1998@gmail.com>
2020-10-07 23:22:58 +0530

    Location tracking app

--- tris-main-master/app/src/main/java/com/example/tris/MainActivity.java
+++ tris-main-master/app/src/main/java/com/example/tris/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_main);
 //Check whether GPS tracking is enabled//
 
         LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
