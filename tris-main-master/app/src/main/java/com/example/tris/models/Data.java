a6c4ec07324f32ebda4acc4721ec2a44e21f3b91
pulkitmahajan23 <pulkitmahajan1998@gmail.com>
2020-10-27 08:46:23 +0530

    Firebase write operation

---
+++ tris-main-master/app/src/main/java/com/example/tris/models/Data.java
@@ -0,0 +1,22 @@
+package com.example.tris.models;
+
+public class Data {
+    public  String data;
+
+    public Data(String data)
+    {
+        this.data=data;
+    }
+
+    public  Data()
+    {}
+
+    public String getData()
+    {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
---
+++ tris-main-master/app/src/main/java/com/example/tris/models/temperatureFire.java
@@ -0,0 +1,24 @@
+package com.example.tris.models;
+
+public class temperatureFire {
+    public String temperatureData;
+
+    public temperatureFire()
+    {}
+
+    public temperatureFire(String temperatureData)
+    {
+        this.temperatureData=temperatureData;
+    }
+
+    public String getTemperatureData()
+    {
+        return temperatureData;
+    }
+
+    public void setTemperatureData(String temperatureData)
+    {
+        this.temperatureData=temperatureData;
+    }
+
+}
